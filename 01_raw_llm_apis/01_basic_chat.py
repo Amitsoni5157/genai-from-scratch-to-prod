f569245aec8a865075af893e6e56bd3a5d9d41b5
Amit Soni <amit.soni5157@gmail.com>
2026-07-10 18:19:57 +0530

    remove unwanted comment

--- 01_raw_llm_apis/01_basic_chat.py
+++ 01_raw_llm_apis/01_basic_chat.py
@@ -40,33 +40,43 @@ import os
 from groq import Groq
 
 
+# load your env variables 
 load_dotenv()
 
+# get your api key
 api_key = os.getenv("GROQ_API_KEY")
 
+# safety check 
 if api_key is None:
     print("Groq API Key is not set. Please set it in the .env file.")
     exit()
 
+# initlize your client 
 client = Groq(api_key=api_key)
 
+# call your LLM model
 response = client.chat.completions.create(
+    # which model you want to use 
     model = "llama-3.3-70b-versatile",
     messages = [
+        # system prompt : sets the behavior of the model 
         {
             "role": "system",
             "content": "You are a programming tutor. Keep your answers concise, structured, and strictly within 3 sentences"
         },
+        # user message : the actual prompt you send to the model 
         {
             "role": "user",
             "content":  "Why is Python so popular for ML?"
         }
     ],
-
+    # set the temperature of the model (creativity)
     temperature = 0.2,
+    # set the max tokens of the model (length of response)
     max_tokens = 150
 )
 
+# print the response 
 print(response.choices[0].message.content)
 
 # Token which you sent to the model 
